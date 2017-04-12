e4301473b5ec4c261c78bc4e5911b8e0666e5cf7
Jon Pither <jon.pither@gmail.com>
2017-04-12 21:39:36 +0100

    Fixes #26, wasn't a bug, but now we have a covering test

--- test/mach/core_test.cljs
+++ test/mach/core_test.cljs
@@ -2,5 +2,10 @@
   (:require [cljs.test :refer [deftest is testing]]
             [mach.core :as mach]))
 
-(deftest test-hello-world
-  (is true))
+(deftest test-order-dedup-deps
+  (is (= (list 'foo 'tar 'bar) (mach/order {'bar  nil
+                                            'tar {'depends ['bar]
+                                                  'update! nil}
+                                            'foo {'depends ['tar 'bar]
+                                                  'update! nil}}
+                                           'foo))))

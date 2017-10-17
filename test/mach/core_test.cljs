51c233e47953791da5ce732df5bf44d4c61c3811
Nicolas Berger <nicoberger@gmail.com>
2017-10-16 22:38:43 -0300

    Fix test: Call target-order instead of order

--- test/mach/core_test.cljs
+++ test/mach/core_test.cljs
@@ -3,9 +3,9 @@
             [mach.core :as mach]))
 
 (deftest test-order-dedup-deps
-  (is (= (list 'foo 'tar 'bar) (mach/order {'bar  nil
-                                            'tar {'depends ['bar]
-                                                  'update! nil}
-                                            'foo {'depends ['tar 'bar]
-                                                  'update! nil}}
-                                           'foo))))
+  (is (= (list 'foo 'tar 'bar) (mach/target-order {'bar nil
+                                                   'tar {'depends ['bar]
+                                                         'update! nil}
+                                                   'foo {'depends ['tar 'bar]
+                                                         'update! nil}}
+                                                  'foo))))
